8b575e56db30ece642e909cce80b69b41c5aac7a
Pandu Ananto <pandunih@gmail.com>
2020-08-27 20:27:11 +0700

    navbar done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,6 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
+import "../styles/responsive.css";
+import "../scripts/components/nav-bar/NavBar.js"
 
 console.log("Hello Coders! :)");
--- webpack.common.js
+++ webpack.common.js
@@ -31,8 +31,8 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src/public/"),
-          to: path.resolve(__dirname, "dist/"),
+          from: path.resolve(__dirname, "src/assets/"),
+          to: path.resolve(__dirname, "dist/assets/"),
         },
       ],
     }),
